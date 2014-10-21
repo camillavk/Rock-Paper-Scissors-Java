56356781027b38b81e21cce15e32fb256782e940
Camilla van Klinken <camillavk@gmail.com>
2014-10-21 16:32:25 +0100

    ...

--- server.rb
+++ server.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
+require 'sinatra/partial'
 
 set :public_dir, Proc.new { File.join(root, "public")}
 set :public_folder, 'public'
+
+set :partial_template_engine, :erb
 
 get '/' do
 	erb :index
