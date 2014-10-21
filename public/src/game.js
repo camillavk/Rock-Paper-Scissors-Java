e5b6bff965b77fcf81f86b7c919db6ee338f5348
Camilla van Klinken <camillavk@gmail.com>
2014-10-21 11:51:43 +0100

    more refactoring

--- public/src/game.js
+++ public/src/game.js
@@ -2,16 +2,9 @@ var game = new Game
 
 	var gest = {Rock: new Rock, Paper: new Paper, Scissors: new Scissors, Spock: new Spock, Lizard: new Lizard};
 
-// var computer = function() {
-// 	var gestures = [new Rock, new Paper, new Scissors, new Spock, new Lizard];
-// 	return gestures[Math.floor(Math.random()*gestures.length)]
-// };
-
 function Winner(winner) {
 	$('h2').text("The winner is " + winner);
 }
-
-// var playersChoice = gest[$(this).data('gesture')]
 
 var computersChoice = function() {
 	var gestures = [new Rock, new Paper, new Scissors, new Lizard, new Spock];
@@ -19,20 +12,8 @@ var computersChoice = function() {
 };
 
 $('img').on('click', function() {
-		// console.log($(this).data('gesture'))
-		// console.log(gest[$(this).data('gesture')])
 		var choiceOne = gest[$(this).data('gesture')]
-		// console.log(choiceOne.type)
 		var choiceTwo = computersChoice()
-		// console.log(choiceTwo.type)
-		// var winner = function() {}
-		// console.log(game.winningChoiceOf(choiceOne, choiceTwo))
 		var winner = game.winningChoiceOf(choiceOne, choiceTwo)
-		console.log(winner)
 		Winner(winner.type)
-
-
-	// winner(game.winningChoiceOf(console.log((gest[$(this).data('gesture')]), computersChoice())))
 });
-
-// $('img').on('click', function() {alert("Cat")});
